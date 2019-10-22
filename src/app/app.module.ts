ec6b41b13735442fc6118ae2cebd10e76a10fa78
GuillaumeLeNeindre <guillaume.le.n@gmail.com>
2019-10-22 14:58:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { HeaderComponent } from './header/header.component';
+import { DistributionListComponent } from './distribution-list/distribution-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent,
+    DistributionListComponent
   ],
   imports: [
     BrowserModule,
